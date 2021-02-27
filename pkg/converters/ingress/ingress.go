e9fbce67c3aa697b0683d923ab0a11b44c89d10b
Joao Morais <jcmoraisjr@gmail.com>
2021-02-27 10:43:48 -0300

    Fix initial weight configuration
    Weights can be used to distribute distinct load to distinct servers in
    the same backend. An initial value greater the default 1 (one) can be
    configured so agents that changes server weight can properly work and
    adjust the weight in lower granularity.
    
    The initial weight configuration isn't properly working when declared in
    the ingress resource because its usage occurs before the value is
    actually read from the ingress annotation. It works however when
    declared in the service - service has priority and is read in the same
    block the backend is created and configured with initial weight and
    other defaults. Moving down the reading just before starting the
    endpoint configuration fixes this.

--- pkg/converters/ingress/ingress.go
+++ pkg/converters/ingress/ingress.go
@@ -265,7 +265,6 @@ func (c *converter) addBackend(source *annotations.Source, hostpath, fullSvcName
 			Type:      "service",
 		}, hostpath, ann)
 		c.backendAnnotations[backend] = mapper
-		backend.Server.InitialWeight = mapper.Get(ingtypes.BackInitialWeight).Int()
 	}
 	// Merging Ingress annotations
 	conflict := mapper.AddAnnotations(source, hostpath, ann)
@@ -275,6 +274,7 @@ func (c *converter) addBackend(source *annotations.Source, hostpath, fullSvcName
 	}
 	// Configure endpoints
 	if !found {
+		backend.Server.InitialWeight = mapper.Get(ingtypes.BackInitialWeight).Int()
 		switch mapper.Get(ingtypes.BackBackendServerNaming).Value {
 		case "ip":
 			backend.EpNaming = hatypes.EpIPPort
