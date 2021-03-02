463219a340565b133f8eb86ae989d3ca931c2cee
Joao Morais <jcmoraisjr@gmail.com>
2021-03-02 10:27:54 -0300

    fix incorrect reload if endpoint list grows
    Dynamic updates need to check the size of the endpoint list size. Up
    to now the backend server list cannot dynamically grow, even when using
    DNS updates. HAProxy Ingress has a list of empty slots to dynamically
    update endpoints, forcing a reload only when no new empty slots can be
    found.
    
    Kubernetes' ep based updates always rebalance new endpoints with empty
    slots so its size is at least as big as the old one, but DNS based only
    do this if a reload is needed. This update rebalance the new list, so
    future DNS based dynamic updates can work without needing to reload
    haproxy.

--- pkg/haproxy/dynupdate_test.go
+++ pkg/haproxy/dynupdate_test.go
@@ -605,6 +605,26 @@ set server _default_backend/srv001 state ready
 set server _default_backend/srv001 weight 1`,
 			logging: `INFO-V(2) updated endpoint '172.17.0.3:8080' weight '1' state 'ready' on backend/server '_default_backend/srv001'`,
 		},
+		// 24
+		{
+			doconfig1: func(c *testConfig) {
+				b := c.config.Backends().AcquireBackend("default", "app", "8080")
+				b.Resolver = "k8s"
+				b.AcquireEndpoint("172.17.0.2", 8080, "")
+				b.AddEmptyEndpoint()
+			},
+			doconfig2: func(c *testConfig) {
+				b := c.config.Backends().AcquireBackend("default", "app", "8080")
+				b.Resolver = "k8s"
+				b.Dynamic.DynUpdate = true
+				b.AcquireEndpoint("172.17.0.3", 8080, "")
+			},
+			expected: []string{
+				"srv001:172.17.0.3:8080:1",
+				"srv002:127.0.0.1:1023:1",
+			},
+			dynamic: true,
+		},
 	}
 	for i, test := range testCases {
 		c := setup(t)
