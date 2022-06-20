e4ae1db7429b5b4094af5e27541e6b0092aa7fa6
Wojciech Dubiel <wolf@cosmose.co>
2022-06-20 14:34:16 +0200

    don't allow empty origins in cors-allow-origin
    If the first origin (used for setting the header when origin doesn't match)
    is an empty string, it causes haproxy config parsing to fail,
    because `http-request set-header` requires two non-empty strings as arguments.
    
    Also the Origin http header shouldn't contain empty string so there's
    no reason to match for it.

--- pkg/converters/ingress/annotations/validator.go
+++ pkg/converters/ingress/annotations/validator.go
@@ -33,7 +33,7 @@ type validate struct {
 }
 
 var (
-	corsOriginRegex  = regexp.MustCompile(`^(https?://[A-Za-z0-9\-\.]*(:[0-9]+)?|\*)?$`)
+	corsOriginRegex  = regexp.MustCompile(`^(https?://[A-Za-z0-9\-\.]*(:[0-9]+)?|\*)$`)
 	corsMethodsRegex = regexp.MustCompile(`^([A-Za-z]+,?\s?)+$`)
 	corsHeadersRegex = regexp.MustCompile(`^([A-Za-z0-9\-\_]+,?\s?)+$`)
 )
