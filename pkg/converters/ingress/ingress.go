14d4ddf75a751da1fbdb5bc1fb98de42ea08f49f
Joao Morais <jcmoraisjr@gmail.com>
2023-02-08 08:57:55 -0300

    fix path scoped annotation on service resources
    Backend scoped annotations apply just once per service resource, and
    have the most precedence. With this behavior in mind, service
    annotations were being applied along with the annotation mapper
    creation. This works nice with backend scoped annotations.
    
    Path scoped, however, has the ability to apply distinct values to the
    same backend. When path scoped annotations are applied via a service
    resource, only the first path is being applied, along with annotation
    mapper creation. This update ensures that the service annotations are
    applied on every pathlink that points to it. This change will spread
    service annotations over all referenced pathlinks, however this doesn't
    end up with duplicated configurations because the config builder already
    deduplicates configurations on the same key, with the same value, to the
    same backend. Warning on annotation conflict isn't a problem as well, a
    conflict is only warned on distinct values.

--- pkg/converters/ingress/ingress.go
+++ pkg/converters/ingress/ingress.go
@@ -879,16 +879,16 @@ func (c *converter) addBackendWithClass(source *annotations.Source, pathLink hat
 	backend.DNSPort = readDNSPort(svc.Spec.ClusterIP == api.ClusterIPNone, port)
 	mapper, found := c.backendAnnotations[backend]
 	if !found {
-		// New backend, initialize with service annotations, giving precedence
 		mapper = c.mapBuilder.NewMapper()
-		_, _, ann := c.readAnnotations(source, svc.Annotations)
-		mapper.AddAnnotations(&annotations.Source{
-			Namespace: namespace,
-			Name:      svcName,
-			Type:      "service",
-		}, pathLink, ann)
 		c.backendAnnotations[backend] = mapper
 	}
+	// Starting with service annotations, giving precedence
+	_, _, svcann := c.readAnnotations(source, svc.Annotations)
+	mapper.AddAnnotations(&annotations.Source{
+		Namespace: namespace,
+		Name:      svcName,
+		Type:      "service",
+	}, pathLink, svcann)
 	// Merging Ingress annotations
 	conflict := mapper.AddAnnotations(source, pathLink, ann)
 	if len(conflict) > 0 {
