d4e405e1a488f6b95cdd90310980e3d14e8d6ce7
Joao Morais <jcmoraisjr@gmail.com>
2025-09-10 18:20:11 -0300

    fix reading backend ca certificate from file (#1297)
    TLS backend can be configured by adding a CA secret (verify server's
    certificate) and/or a Crt secret (server verify client's certificate).
    A former controller version was reading the namespace of a secret from
    the ingress that configures the annotation, leading to nil dereference
    when the value is configured globally. This was fixed by adding a new
    step, that verifies if the config is global or an annotation, and if
    the provided name has already a name or just the name of the secret.
    
    This new step however broke the proto://content approach added on the
    get secret code because it happens before getting the secret, and the
    proto://content syntax is incompatible with the ns/name one. This
    failed deployments that read ca or crt from the file system.
    
    Should be merged as far as v0.13.

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -3,10 +3,12 @@ package integration_test
 import (
 	"context"
 	"crypto/sha1"
+	"crypto/tls"
 	"encoding/hex"
 	"encoding/pem"
 	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"testing"
@@ -41,6 +43,8 @@ func TestIntegrationIngress(t *testing.T) {
 	require.NoError(t, err)
 
 	caValid, cakeyValid := framework.CreateCA(t, framework.CertificateIssuerCN)
+	caValidKeyPair, err := tls.X509KeyPair(caValid, cakeyValid)
+	require.NoError(t, err)
 	crtValid, keyValid := framework.CreateCertificate(t, caValid, cakeyValid, framework.CertificateClientCN)
 	secretCA := f.CreateSecret(ctx, t, map[string][]byte{"ca.crt": caValid})
 
@@ -323,9 +327,6 @@ func TestIntegrationIngress(t *testing.T) {
 		assert.Equal(t, reqHeaders, res.EchoResponse.ReqHeaders)
 	})
 
-	// should match wildcard host
-	// should match domain conflicting with wildcard host
-
 	t.Run("should give priority on specific domains over wildcard", func(t *testing.T) {
 		t.Parallel()
 		hostname := framework.RandomHostName()
@@ -589,6 +590,42 @@ func TestIntegrationIngress(t *testing.T) {
 		assert.Equal(t, "tcphost1", conn1.ConnectionState().PeerCertificates[0].Subject.CommonName)
 		require.NoError(t, conn1.Close())
 	})
+
+	t.Run("should configure TLS backend using crt files", func(t *testing.T) {
+		t.Parallel()
+		crtPem, keyPem := framework.CreateCertificate(t, caValid, cakeyValid, "localhost")
+		crt, err := tls.X509KeyPair(crtPem, keyPem)
+		require.NoError(t, err)
+		port := f.CreateHTTPServer(ctx, t, "https1",
+			options.ClientCACertificate(caValidKeyPair.Leaf),
+			options.ServerCertificates([]tls.Certificate{crt}),
+		)
+
+		fileCA, err := os.CreateTemp(framework.LocalFSPrefix, "ca")
+		require.NoError(t, err)
+		err = os.WriteFile(fileCA.Name(), caValid, 0644)
+		require.NoError(t, err)
+
+		fileCrt, err := os.CreateTemp(framework.LocalFSPrefix, "crt")
+		require.NoError(t, err)
+		err = os.WriteFile(fileCrt.Name(), append(crtPem, keyPem...), 0644)
+		require.NoError(t, err)
+
+		svc := f.CreateService(ctx, t, port)
+		_, hostname := f.CreateIngress(ctx, t, svc,
+			options.AddConfigKeyAnnotation(ingtypes.BackBackendProtocol, "https"),
+			options.AddConfigKeyAnnotation(ingtypes.BackSecureCrtSecret, "file://"+fileCrt.Name()),
+			options.AddConfigKeyAnnotation(ingtypes.BackSecureVerifyCASecret, "file://"+fileCA.Name()),
+		)
+
+		res := f.Request(ctx, t, http.MethodGet, hostname, "/", options.ExpectResponseCode(http.StatusOK))
+		assert.True(t, res.EchoResponse.Parsed)
+		assert.Equal(t, "https1", res.EchoResponse.ServerName)
+	})
+
+	// should match wildcard host
+
+	// should match domain conflicting with wildcard host
 
 	// should update status on class update
 
